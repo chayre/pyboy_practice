5eec954d7f068d721a6fc3a9a0e027146143a4f7
Colin Ayres <sea.airs77@gmail.com>
2024-10-17 00:27:38 +0200

    Added game wrapper functionality

--- practice.py
+++ practice.py
@@ -1,5 +1,12 @@
+import pyboy
 from pyboy import PyBoy
-pyboy = PyBoy('PokemonRed.gb')
-while pyboy.tick():
-    pass
+from pyboy.plugins.game_wrapper_pokemon_gen1 import GameWrapperPokemonGen1  
+
+rom_path = "/home/cayres/projects/pyboy_practice/rom/PokemonRed.gb"
+pyboy = PyBoy(rom_path, window="SDL2")
+game_wrapper = pyboy.game_wrapper
+
+while pyboy.tick():  # 3c49
+    game_wrapper.post_tick()
+    print(game_wrapper) 
 pyboy.stop()
